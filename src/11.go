5b7df9afaffeb174f4f535005abda0e63ad5570e
escosioyeshareg <escosioyeshareg@gmail.com>
2025-03-17 06:05:58 +0000

    Add 11.go

---
+++ src/11.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare variables
+	var num1 int = 23
+	var num2 float64 = 5.6
+	var sum float64
+
+	// Calculate the sum
+	sum = num1 + num2
+
+	// Print the result
+	fmt.Println(sum)
+}
